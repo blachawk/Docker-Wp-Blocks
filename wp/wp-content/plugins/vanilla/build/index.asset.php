2b3af3ddab53fc575f87cfdc48ba0d55c0f7366c
Kensley <klewis@ffa.org>
2023-03-13 15:05:42 -0400

    vanilla is now visible as a block from editor page with these settings

--- wp/wp-content/plugins/vanilla/build/index.asset.php
+++ wp/wp-content/plugins/vanilla/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '677e549f4d8c5963023b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c90ebeb0a004cab42da3');
