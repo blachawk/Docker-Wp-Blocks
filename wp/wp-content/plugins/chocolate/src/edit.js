4a9abbefcf039e6478dd20ce4e721ab99bac7772
Kensley <klewis@ffa.org>
2023-03-14 15:38:41 -0400

    making use of BlockControls | but the ordering process is a bit confusing with the group attribute.  In addition if set to "other" nothing shows.

--- wp/wp-content/plugins/chocolate/src/edit.js
+++ wp/wp-content/plugins/chocolate/src/edit.js
@@ -1,5 +1,10 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	RichText,
+	BlockControls,
+} from '@wordpress/block-editor';
+import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
 import './editor.scss';
 
 export default function Edit( { attributes, setAttributes } ) {
@@ -7,13 +12,38 @@ export default function Edit( { attributes, setAttributes } ) {
 
 	// additional options for our RichText component - https://github.com/WordPress/gutenberg/blob/HEAD/packages/block-editor/src/components/rich-text/README.md
 	return (
-		<RichText
-			{ ...useBlockProps() }
-			onChange={ ( value ) => setAttributes( { text: value } ) }
-			value={ text }
-			placeholder={ __( 'My chocolate placeholder', 'chocolate' ) }
-			tagName="h4"
-			allowedFormats={ 'core/bold' }
-		/>
+		<>
+			<BlockControls group="inline">
+				<p>Inline Controls</p>
+			</BlockControls>
+
+			<BlockControls
+				controls={ [
+					{
+						title: 'Button 1',
+						icon: 'admin-generic',
+						isActive: true,
+						onClick: () => console.log( 'Button 1 clicked!' ),
+					},
+					{
+						title: 'Button 2',
+						icon: 'admin-collapse',
+						onClick: () => console.log( 'Button 2 clicked!' ),
+					},
+				] }
+			/>
+			<RichText
+				{ ...useBlockProps() }
+				onChange={ ( value ) => setAttributes( { text: value } ) }
+				value={ text }
+				placeholder={ __( 'My chocolate placeholder', 'chocolate' ) }
+				tagName="h4"
+				allowedFormats={ 'core/bold' }
+			/>
+
+			<BlockControls>
+				<p>Other Controls</p>
+			</BlockControls>
+		</>
 	);
 }
