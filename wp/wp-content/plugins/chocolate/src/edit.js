e2ee73bcd321bbb3ff79f1c9dac76ac8f589314b
Kensley <klewis@ffa.org>
2023-03-13 23:04:01 -0400

    add a richtext editor to our block | edit side

--- wp/wp-content/plugins/chocolate/src/edit.js
+++ wp/wp-content/plugins/chocolate/src/edit.js
@@ -1,11 +1,18 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, RichText } from '@wordpress/block-editor';
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
+	const { text } = attributes;
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Chocolate – hello from the editor!', 'chocolate' ) }
-		</p>
+		<RichText
+			{ ...useBlockProps() }
+			onChange={ ( value ) => setAttributes( { text: value } ) }
+			value={ text }
+			placeholder={ __( 'My chocolate placeholder', 'chocolate' ) }
+			tagName="h4"
+			allowedFormats={ 'core/bold' }
+		/>
 	);
 }
