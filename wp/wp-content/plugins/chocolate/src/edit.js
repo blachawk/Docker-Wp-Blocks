0d83c8c5629c0e99eca64fd963c3b47d55542215
Kensley <klewis@ffa.org>
2023-03-15 18:05:01 -0400

    optimizing code to have simplicity of block support colors to stand out | should still work the same on back and front | useBlockProps and block.json supports are working together to make this process simple.

--- wp/wp-content/plugins/chocolate/src/edit.js
+++ wp/wp-content/plugins/chocolate/src/edit.js
@@ -3,32 +3,13 @@ import {
 	useBlockProps,
 	RichText,
 	BlockControls,
-	InspectorControls,
 	AlignmentToolbar,
-	PanelColorSettings,
-	ContrastChecker,
-	withColors,
 } from '@wordpress/block-editor';
-import {
-	ToolbarGroup,
-	ToolbarButton,
-	ToolbarDropdownMenu,
-	TextControl,
-	TextareaControl,
-	ToggleControl,
-	ColorPicker,
-} from '@wordpress/components';
+
 import './editor.scss';
 
-function Edit( props ) {
-	const {
-		attributes,
-		setAttributes,
-		backgroundColor,
-		textColor,
-		setBackgroundColor,
-		setTextColor,
-	} = props;
+export default function Edit( props ) {
+	const { attributes, setAttributes } = props;
 	const { text, alignment } = attributes;
 
 	//test | lets now see values for attributes and props
@@ -44,103 +25,11 @@ function Edit( props ) {
 	// additional options for our RichText component - https://github.com/WordPress/gutenberg/blob/HEAD/packages/block-editor/src/components/rich-text/README.md
 	return (
 		<>
-			<InspectorControls>
-				<PanelColorSettings
-					title={ __( 'My Color Settings', 'chocolate' ) }
-					icon="admin-appearance"
-					initialOpen
-					disableCustomColors={ false }
-					colorSettings={ [
-						{
-							value: backgroundColor.color,
-							onChange: setBackgroundColor,
-							label: __( 'Background Color', 'chocolate' ),
-						},
-						{
-							value: textColor.color,
-							onChange: setTextColor,
-							label: __( 'Text Color', 'chocolate' ),
-						},
-					] }
-				>
-					<ContrastChecker
-						textColor={ textColor.color }
-						backgroundColor={ backgroundColor.color }
-					/>
-
-					<TextControl
-						label="Input Label"
-						value={ text }
-						onChange={ onChangeText }
-						help="my help text"
-					/>
-					<TextareaControl
-						label="Text Area Label"
-						value={ text }
-						onChange={ onChangeText }
-						help="my help text v2"
-					/>
-					<ToggleControl
-						label="Toggle Label"
-						checked={ true }
-						onChange={ ( v ) => console.log( v ) }
-					/>
-					<ColorPicker
-						color={ 'F03' }
-						onChangeComplete={ ( v ) => console.log( v ) }
-					/>
-				</PanelColorSettings>
-			</InspectorControls>
 			<BlockControls>
 				<AlignmentToolbar
 					value={ alignment }
 					onChange={ onChangeAlignment }
 				/>
-			</BlockControls>
-
-			<BlockControls>
-				<ToolbarGroup>
-					<ToolbarButton
-						title="Button 1"
-						icon="admin-generic"
-						isActive="true"
-						onClick={ () => console.log( 'Button 1' ) }
-					/>
-				</ToolbarGroup>
-				<ToolbarGroup>
-					<ToolbarButton
-						title="Align Left"
-						icon="editor-alignleft"
-						onClick={ () => console.log( 'Align Left' ) }
-					></ToolbarButton>
-					<ToolbarButton
-						title="Align Center"
-						icon="editor-aligncenter"
-						onClick={ () => console.log( 'Align Center' ) }
-					></ToolbarButton>
-					<ToolbarButton
-						title="Align Right"
-						icon="editor-alignright"
-						onClick={ () => console.log( 'Align Right' ) }
-					></ToolbarButton>
-
-					<ToolbarDropdownMenu
-						icon="arrow-down-alt2"
-						label={ __( 'More Alignments', 'chocolate' ) }
-						controls={ [
-							{
-								title: __( 'Wide', 'chocolate' ),
-								icon: 'align-wide',
-								onClick: () => console.log( 'Align Wide' ),
-							},
-							{
-								title: __( 'Full', 'chocolate' ),
-								icon: 'align-full-width',
-								onClick: () => console.log( 'Align Full' ),
-							},
-						] }
-					/>
-				</ToolbarGroup>
 			</BlockControls>
 
 			{ /* https://developer.wordpress.org/block-editor/reference-guides/richtext/#example */ }
@@ -152,17 +41,7 @@ function Edit( props ) {
 				value={ text }
 				placeholder={ __( 'My chocolate placeholder', 'chocolate' ) }
 				tagName="h4"
-				style={ {
-					textAlign: alignment,
-					backgroundColor: backgroundColor.color,
-					color: textColor.color,
-				} }
 			/>
 		</>
 	);
 }
-
-export default withColors( {
-	backgroundColor: 'backgroundColor',
-	textColor: 'color',
-} )( Edit );
--- wp/wp-content/plugins/chocolate/src/save.js
+++ wp/wp-content/plugins/chocolate/src/save.js
@@ -1,38 +1,11 @@
-import {
-	useBlockProps,
-	RichText,
-	getColorClassName,
-} from '@wordpress/block-editor';
+import { useBlockProps, RichText } from '@wordpress/block-editor';
 import classnames from 'classnames';
 
 export default function save( { attributes } ) {
-	const {
-		text,
-		alignment,
-		backgroundColor,
-		textColor,
-		customBackgroundColor,
-		customTextColor,
-	} = attributes;
-
-	//test
-	console.log( attributes );
-
-	const backgroundClass = getColorClassName(
-		'background-color',
-		backgroundColor
-	);
-
-	const textClass = getColorClassName( 'color', textColor );
-
-	//test
-	console.log( backgroundColor, textClass );
+	const { text, alignment } = attributes;
 
 	//improve our usage of class names by using classnames
-	const classes = classnames( `text-box-align-${ alignment }`, {
-		[ backgroundClass ]: backgroundClass,
-		[ textClass ]: textClass,
-	} );
+	const classes = classnames( `text-box-align-${ alignment }` );
 
 	//test
 	console.log( classes );
@@ -41,12 +14,6 @@ export default function save( { attributes } ) {
 		<RichText.Content
 			{ ...useBlockProps.save( {
 				className: classes,
-				style: {
-					backgroundColor: backgroundClass
-						? undefined
-						: customBackgroundColor,
-					color: textClass ? undefined : customTextColor,
-				},
 			} ) }
 			tagName="h4"
 			value={ text }
