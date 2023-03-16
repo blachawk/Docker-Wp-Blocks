73d6231ce26b0b2f4c48566562466b8f1720b0cd
Kensley <klewis@ffa.org>
2023-03-16 11:17:07 -0400

    how to create variations of our existing block with unique attributes

--- wp/wp-content/plugins/chocolate/src/index.js
+++ wp/wp-content/plugins/chocolate/src/index.js
@@ -3,6 +3,7 @@ import './style.scss';
 import Edit from './edit';
 import save from './save';
 import metadata from './block.json';
+import { __ } from '@wordpress/i18n';
 
 registerBlockType( metadata.name, {
 	icon: {
@@ -12,4 +13,24 @@ registerBlockType( metadata.name, {
 	},
 	edit: Edit,
 	save,
+	variations: [
+		{
+			name: 'course-blocks/gradient-chocolate-a',
+			title: __( 'Gradient Variations of Chocolate A' ),
+			icon: 'wordpress',
+			attributes: {
+				gradient: 'red-to-blue',
+				shadow: true,
+			},
+		},
+		{
+			name: 'course-blocks/gradient-chocolate-b',
+			title: __( 'Gradient Variations of Chocolate B' ),
+			icon: 'welcome-learn-more',
+			attributes: {
+				gradient: 'green-to-yellow',
+				shadow: true,
+			},
+		},
+	],
 } );
