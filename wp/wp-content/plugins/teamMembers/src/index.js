d0c8b0e11a2959c40f2a4e25445664ae753f227b
Kensley <klewis@ffa.org>
2023-03-23 13:47:32 -0400

    how to transform an existing gutenberg gallery of photos into a team members block

--- wp/wp-content/plugins/teamMembers/src/index.js
+++ wp/wp-content/plugins/teamMembers/src/index.js
@@ -1,9 +1,10 @@
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType, createBlock } from '@wordpress/blocks';
 import './team-member';
 import './style.scss';
 import Edit from './edit';
 import save from './save';
 import metadata from './block.json';
+import { InnerBlocks } from '@wordpress/block-editor';
 
 registerBlockType(metadata.name, {
 	icon: {
@@ -13,4 +14,28 @@ registerBlockType(metadata.name, {
 	},
 	edit: Edit,
 	save,
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				blocks: ['core/gallery'],
+				transform: ({ images, columns }) => {
+					const innerBlocks = images.map(({ url, id, alt }) => {
+						return createBlock('course-blocks/team-member', {
+							alt,
+							id,
+							url,
+						});
+					});
+					return createBlock(
+						metadata.name,
+						{
+							columns: columns || 2,
+						},
+						innerBlocks
+					);
+				},
+			},
+		],
+	},
 });
