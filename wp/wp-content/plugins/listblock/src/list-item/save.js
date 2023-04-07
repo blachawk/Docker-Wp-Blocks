2319c017ed6e6e6300c44f97503d170522bf4b48
Kensley <klewis@ffa.org>
2023-04-07 12:45:54 -0400

    updatec comment

--- wp/wp-content/plugins/listblock/src/list-item/save.js
+++ wp/wp-content/plugins/listblock/src/list-item/save.js
@@ -2,7 +2,7 @@ import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 
 export default function Save() {
 	const blockProps = useBlockProps.save( {
-		//we don't need this but its cool to know
+		//don't need this right now
 		//className: 'my-additional-list-item-class',
 	} );
 
